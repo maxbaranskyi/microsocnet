646971c5ae9d5201c2ff379f64091a12f7dfcd68
Max Baranskyi <max.baranskyi@gmail.com>
2022-08-15 18:27:05 +0300

    Migration for profile table

---
+++ database/migrations/2022_08_15_144702_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('description', 160);
+            $table->string('location', 30);
+            $table->string('website', 100);
+            $table->date('birthday');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
