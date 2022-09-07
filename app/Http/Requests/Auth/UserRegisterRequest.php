33f1c7c03739642d634a33939fda2144979f4518
Max Baranskyi <max.baranskyi@gmail.com>
2022-09-07 19:17:45 +0300

    Requests for validating login and registration

---
+++ app/Http/Requests/Auth/UserRegisterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:50',
+            'username' => 'required|max:15',
+            'email' => 'required|email:rfc,dns|unique:users,email',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password'
+        ];
+    }
+}
